84a3e2fb102519318f1417b77bc7a35382272758
gaytanescudero <gaytanescudero21@outlook.com>
2025-12-02 16:32:57 -0600

    Refactor code structure for improved readability and maintainability

--- src/views/layout/header.php
+++ src/views/layout/header.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <title>Sistema Jardinería</title>
-    <link href ="/assets/css/estilos_jardineria.css" rel="stylesheet" >
+    <link href="/assets/css/estilos_jardineria.css" rel="stylesheet">
     <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
-
 </head>
+
 <body>
+
 <header class="header-jardin">
+
+    <!-- Aquí va la imagen -->
+    <img src="/assets/img/logo_jardin.png" alt="Logo Jardinería" class="logo-jardin">
+
     <h1>Sistema de Gestión Jardines Tamoanchan</h1>
     <div class="header-subtitle">Cultivando excelencia en cada detalle</div>
 </header>
-
 
